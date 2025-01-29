f12cb7fae9a3709e255d5add56f6263a03ee39c0
Kristina Fokina <kristinalazareva1993@yandex.ru>
2025-01-29 10:47:21 +0700

    Стили для шрифтов

--- src/app/fonts.tsx
+++ src/app/fonts.tsx
@@ -2,14 +2,14 @@ import { Inter, Roboto } from "next/font/google";
 
 //Основной текст, подписи и мелкий текст, текст в кнопках
 export const inter = Inter({
-  subsets: ["latin"],
+  subsets: ["cyrillic"],
   weight: ["400", "700"], // 400 - Regular, 700 - Bold
   display: "swap",
 });
 
 //Заголовки
 export const roboto = Roboto({
-  subsets: ["latin"],
+  subsets: ["cyrillic"],
   weight: ["700"], // 700 - Bold
   display: "swap",
 });
