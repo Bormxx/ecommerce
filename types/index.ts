77bfe4f4f736519a8b8f6ec1ef52df6f52e762d8
Bormxx <m2315226@edu.misis.ru>
2025-02-25 18:48:10 +0300

    Субботние чтения

--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,36 @@
+import { z } from "zod";
+
+export const authFormSchema = z.object({
+  email: z.string().min(1, 'Введите email').email(),
+  password: z.string().min(6, 'Введите пароль'),
+});
+
+export type TAuthForm = z.infer<typeof authFormSchema>
+
+export const registrFormSchema = z.object({
+  name: z.string().min(2, 'Введите имя'),
+  surname: z.string().min(2, 'Введите фамилию'),
+  email: z.string().min(1, 'Введите email').email(),
+  password: z.string().min(6, 'Введите пароль'),
+  passwordCompare: z.string().min(6)
+}).refine(
+  (data) => data.password === data.passwordCompare, {
+    message: "don't match",
+    path: ['passwordCompare']
+  }
+);
+
+export type TRegistrForm = z.infer<typeof registrFormSchema>
+
+export const formDataSchema = z.object({
+  name: z.string().min(2, 'Введите имя'),
+  surname: z.string().min(2, 'Введите фамилию'),
+  email: z.string().min(1, 'Введите email').email(),
+  password: z.string().min(6, 'Введите пароль'),
+})
+
+export type TFormData = z.infer<typeof formDataSchema>
+
 export interface Items {
   id: number;
   title: string;
