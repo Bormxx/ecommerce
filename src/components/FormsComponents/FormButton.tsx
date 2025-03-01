3139c4afb444c9eef2baa69da305aa60e04e5116
MAG_It_student <tc4evlad@yandex.ru>
2025-03-02 01:09:02 +0300

    added killer feature to hover on disabled button

--- src/components/FormsComponents/FormButton.tsx
+++ src/components/FormsComponents/FormButton.tsx
@@ -10,7 +10,7 @@ export default function FormButton(prop: FormButtonProp) {
   return (
     <Button
       type="submit"
-      className={`${inter.className} rounded-md bg-blue-800 px-4 py-3 text-center text-base font-bold text-white hover:bg-blue-600 disabled:bg-slate-400`}
+      className={`${inter.className} rounded-md bg-blue-800 px-4 py-3 text-center text-base font-bold text-white hover:bg-blue-600 disabled:bg-slate-400 disabled:cursor-no-drop`}
       disabled={prop.isValid ? false : true}
     >
       {prop.text}
