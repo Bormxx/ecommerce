fe131b9e578ae6bab5941a7d4478d456845c4054
Bormxx <m2315226@edu.misis.ru>
2025-03-02 10:57:29 +0300

    Воскресное бдение

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import MainSection from "@/components/MainSection/MainSection";
 import { Items, Photos } from "../../types";
 import { useUserStore } from "@/store/auth";
+import HomeContainer from "../components/HomeContainer/HomeContainer";
+import ProtectedRoute from "../components/ProtectedRoute/ProtectedRoute";
+import { useProtectedRoute } from "../hooks/useProtectedRoute";
 
 interface ItemsList {
   request: Items[];
@@ -21,16 +24,18 @@ export default function Home({ items, photos }: TypeRequest) {
   return (
 
     <HomeContainer>
-      <MainSection items={items} photos={photos} />
-      {isAuthenticated && <h1>Hellow</h1>}
+      <ProtectedRoute protection={useProtectedRoute}>
+        <MainSection items={items} photos={photos} />
+        {isAuthenticated && <h1>Hellow</h1>}
+      </ProtectedRoute>
     </HomeContainer>
   );
 }
 
 export async function getStaticProps() {
-  const itemsRes = await fetch("/api/items");
+  const itemsRes = await fetch("http://127.0.0.1:3000/api/items");
   const items = await itemsRes.json();
-  const photosRes = await fetch("/api/photos");
+  const photosRes = await fetch("http://127.0.0.1:3000/api/photos");
   const photos = await photosRes.json();
   return {
     props: { items, photos },
