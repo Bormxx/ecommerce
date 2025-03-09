a9f6188fb26c80589adfc7f01c499862c6f391dc
MikleKuzichev <dev310105@gmail.com>
2025-03-09 12:23:06 +0300

    Fix: Edit link in sidebar component

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,23 +1,27 @@
+import Link from "next/link";
+
 export default function Sidebar() {
   const menuItems = [
-    { text: "Мой профиль", active: true },
-    { text: "История заказов" },
-    { text: "Товары в избранном" },
-    { text: "Корзина" },
+    { text: "Мой профиль", href: "/profile", active: true },
+    { text: "История заказов", href: "/orders" },
+    { text: "Товары в избранном", href: "/favorites" },
+    { text: "Корзина", href: "/cart" },
   ];
 
   return (
     <aside className="min-w-[264px] pr-4 border-r-[1px]">
         <ul className="text-[#1F2937]">
-          {menuItems.map(({ text, active }) => (
-            <li
-              key={text}
-              className={`cursor-pointer hover:bg-[#F3F4F6] mb-2 pl-4 pr-4 pb-2 pt-2 rounded-lg ${
+          {menuItems.map(({ text, href, active }) => (
+            <li key={text} className="mb-2">
+            <Link
+              href={href}
+              className={`block pl-4 pr-4 pb-2 pt-2 rounded-lg cursor-pointer hover:bg-[#F3F4F6] ${
                 active ? "text-[#2563EB] font-medium" : ""
               }`}
             >
               {text}
-            </li>
+            </Link>
+          </li>
         ))}
         </ul>
     </aside>
