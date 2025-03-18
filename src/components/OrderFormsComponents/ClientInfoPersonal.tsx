7945c1b74170dea2392dad8187819aef0fc0a5ed
MAG_It_student <tc4evlad@yandex.ru>
2025-03-18 20:04:50 +0300

    showing user data on order confirm page

--- src/components/OrderFormsComponents/ClientInfoPersonal.tsx
+++ src/components/OrderFormsComponents/ClientInfoPersonal.tsx
@@ -1,14 +1,17 @@
+import { useUserStore } from "@/shared/store/auth";
 import { cn } from "@/shared/utils/frontend/cn";
 import { inter } from "@/styles/fonts";
 
 export default function ClientInfoPersonal() {
+  const {name, surname, email} = useUserStore();
+
   return (
     <div className="flex flex-col gap-2">
       <p className={cn(inter.className, "text-base font-normal")}>
-        Ярополк Филибусов
+        {name + " " + surname}
       </p>
       <p className={cn(inter.className, "text-sm font-normal text-gray-500")}>
-        email@email.ru
+        {email}
       </p>
     </div>
   );
