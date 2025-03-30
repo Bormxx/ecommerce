d78012af20b699f56a70bf5d12ed572013784914
Kristina Fokina <kristinalazareva1993@yandex.ru>
2025-03-30 11:45:50 +0300

    Поиск работает на полное соответствие

--- src/components/HeaderComponent/HeaderComponent.tsx
+++ src/components/HeaderComponent/HeaderComponent.tsx
@@ -5,16 +5,38 @@ import { useRouter } from "next/router";
 import Link from "next/link";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { TItemSchema } from "@/shared/types/schemas/item";
+import SearchField from "../AuthFormsComponents/SearchField";
+
+
 export default function HeaderComponent() {
   const { pathname } = useRouter();
+  const hiddenRoutes = ["/cart", "/profile", "/order"];
   const router = useRouter();
-  const hiddenRoutes = ["/cart", "/profile"];
+
+  const itemsList: TItemSchema[] = [];
+
+  const [query, setQuery] = useState("");
+  const [items, setItems] = useState(itemsList);
+
+  const { control } = useForm<{ input: string }>({
+    mode: "all",
+  });
+
   const hiddenInCart = hiddenRoutes.includes(pathname) ? "hidden" : "";
   function routerToCatalog() {
     router.replace("/catalog");
   }
   return (
-    <>
+    <div
+      className={
+        pathname === "/auth" || pathname === "/registration"
+          ? "hidden md:block"
+          : ""
+      }
+    >
       <header className="hidden w-full bg-white p-3 shadow-custom md:flex">
         <div className="m-auto flex max-w-[1180px] flex-grow justify-between gap-2 lg:gap-7">
           <Link href="/">
@@ -34,9 +56,13 @@ export default function HeaderComponent() {
               Каталог
             </button>
             <form action="" className="flex flex-grow">
-              <input
-                type="text"
-                className="flex-grow rounded-bl-[8px] rounded-tl-[8px] border-2 border-[#1E40AF] bg-white p-2"
+              <SearchField
+                name={"input"}
+                control={control}
+                query={query}
+                items={items}
+                setQuery={setQuery}
+                setItems={setItems}
               />
               <button
                 type="submit"
@@ -58,19 +84,19 @@ export default function HeaderComponent() {
 
       {/* Верхняя часть при маленьком экране */}
       <div className={`relative ${hiddenInCart} m-5 flex h-[36px] md:hidden`}>
-        <input
-          type="text"
-          placeholder="Искать"
-          className="flex-grow rounded-[8px] border-2 border-[#1E40AF] bg-white p-2.5 pl-[36px]"
+        <SearchField
+          name={"input"}
+          control={control}
+          query={query}
+          items={items}
+          setQuery={setQuery}
+          setItems={setItems}
         />
-        <div className="absolute left-0 top-0 p-2.5">
-          <MagnifyingGlassIcon width={16} height={16} />
-        </div>
       </div>
       {/* Нижняя часть при маленьком экране */}
       <div className="fixed bottom-0 left-0 z-50 flex w-full items-center justify-center bg-white p-4 md:hidden">
         <LoginMenu />
       </div>
-    </>
+    </div>
   );
 }
---
+++ src/pages/api/searchProducts/index.ts
@@ -0,0 +1,31 @@
+import { db } from "@/db";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function searchProducts(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method === "POST") {
+    const { query } = req.body;
+
+    if (!query) {
+      return res.status(400).json({ error: "Запрос не может быть пустым" });
+    }
+
+    try {
+      const items = await db.query.items.findMany({
+        where: (item, { like }) => like(item.title, `%${query}%`),
+      });
+
+      return res.status(200).json(items);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: "Ошибка при выполнении поиска" });
+    }
+  } else {
+    res.setHeader("Allow", ["POST"]);
+    return res
+      .status(405)
+      .end(`Метод ${req.method} не разрешен. Используйте метод POST.`);
+  }
+}
