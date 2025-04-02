64fe86dcb7196a7ea1b89cc3609bc6a906ecc9f8
MikleKuzichev <dev310105@gmail.com>
2025-04-02 21:53:34 +0300

    Feat: Add new layout product

--- src/components/ProductPage/ProductPage.tsx
+++ src/components/ProductPage/ProductPage.tsx
@@ -1,19 +1,10 @@
 import {
     AdjustmentsHorizontalIcon,
-    ArrowLongLeftIcon,
-    XMarkIcon,
   } from "@heroicons/react/24/outline";
   import Breadcrumbs from "../Breadcrumbs/Breadcrumbs";
-  import Title from "../Title/Title";
   import { useState } from "react";
   import { inter, roboto } from "@/styles/fonts";
-  import FilterComponent from "../FilterComponent/FilterComponent";
-  import FilterPrice from "../FilterComponent/FilterPrice";
-  import FilterCheckbox from "../FilterComponent/FilterCheckbox";
-  import FilterRadio from "../FilterComponent/FilterRadio";
-  import FilterSwitch from "../FilterComponent/FilterSwitch";
-  import ButtonLong from "../ui-kit/ButtonLong";
-  import CatalogList from "../CatalogList/CatalogList";
+  import Image from 'next/image';
   import { Photos, TItems } from "@/shared/types";
   import { useRouter } from "next/router";
   export interface TypeRequest {
@@ -29,84 +20,115 @@ import {
         ? "Очки " + id.charAt(0).toUpperCase() + id.slice(1)
         : ""
       : "Каталог";
-    const [isOpenFilterClass, setIsOpenFilterClass] = useState("hidden");
-  
-    function clickOpenFilter() {
-      setIsOpenFilterClass("flex");
-    }
-  
-    function clickCloseFilter() {
-      setIsOpenFilterClass("hidden");
-    }
-    function handleFilter() {}
-  
+
     return (
       <>
         <Breadcrumbs />
-        <div className="gap-5 md:flex">
-          <div
-            className={`${isOpenFilterClass} fixed left-0 top-0 z-[51] h-full w-full flex-col gap-4 overflow-y-auto bg-gray-50 px-5 py-6 md:static md:flex md:w-auto md:overflow-y-visible md:p-0`}
-          >
-            <div className="flex gap-1 md:hidden">
-              <button type="button" onClick={clickCloseFilter}>
-                <ArrowLongLeftIcon width={24} height={24} />
-              </button>
-              <h2
-                className={`${roboto.className} text-2xl font-bold text-gray-800 md:text-3xl`}
-              >
-                Фильтр
-              </h2>
-            </div>
-            <form className="flex flex-col gap-4 md:w-72">
-              <div className="flex flex-col gap-4">
-                <FilterComponent title="Цена" content={<FilterPrice />} />
-                <FilterComponent title="Цвет" content={<FilterCheckbox />} />
-                <FilterComponent title="Наличие" content={<FilterRadio />} />
-                <FilterComponent
-                  title="Еще какой-нибудь фильтр"
-                  content={<FilterSwitch />}
-                />
-              </div>
-              <div className="flex w-full">
-                <ButtonLong
-                  text="Применить фильтр"
-                  onClick={handleFilter}
-                  type="submit"
-                />
-              </div>
-            </form>
+        <div className="relative my-4 flex flex-wrap justify-evenly gap-4 md:rounded-xl md:bg-white md:p-6 md:shadow-custom">
+          <div className="gap-5 md:flex md:justify-center">
+
+          <div>
+          <div className="w-[456px] h-[460px] bg-white rounded-lg overflow-hidden flex items-center justify-center">
+            <Image 
+              src="/images/product_for_dev.png" 
+              alt="Очки классные" 
+              width={456} 
+              height={460} 
+              className="w-full h-full object-cover" 
+            />
           </div>
-          <main className="w-full">
-            <div className="flex justify-between">
-              <Title text={categoryName} />
-              <button
-                type="button"
-                onClick={clickOpenFilter}
-                className="md:hidden"
-              >
-                <AdjustmentsHorizontalIcon width={24} height={24} />
-              </button>
-            </div>
-  
-            <div className="relative my-4 flex flex-wrap justify-evenly gap-4 md:mt-11 md:rounded-xl md:bg-white md:p-6 md:shadow-custom">
-              <div className="absolute left-0 top-[-40px] hidden gap-2 md:flex">
-                <div
-                  className={`${inter.className} flex items-center rounded-[20px] border border-blue-800 px-3 py-[2px] text-xs text-blue-800`}
-                >
-                  Фильтр 1
-                  <XMarkIcon width={24} height={24} className="ml-3" />
+          <div className="flex justify-center gap-2 mt-4">
+                <button className="w-[97px] h-[96px] bg-gray-200 rounded-lg"></button>
+                <button className="w-[97px] h-[96px] bg-gray-200 rounded-lg"></button>
+                <button className="w-[97px] h-[96px] bg-gray-200 rounded-lg"></button>
+                <button className="w-[97px] h-[96px] bg-gray-200 rounded-lg"></button>
+              </div>
+          </div>
+
+            <main className="w-full max-w-[456px]">
+              <div className="flex justify-between">
+                <h1 className="font-bold text-[30px] leading-9 text-[#1F2937]">{categoryName}</h1> 
+
+                <div className="flex flex-col items-center justify-center gap-1">
+                  <div className="flex items-center gap-2">
+                    <Image 
+                      className="w-8 h-8 text-[#2563EB] object-center object-contain" 
+                      src="/images/Star.svg" 
+                      alt="Рейтинг" 
+                      width={32} 
+                      height={32} 
+                    />
+                    <span className="font-bold text-[30px] leading-9 text-[#1F2937]">4.0</span>
+                  </div>
                 </div>
-                <div
-                  className={`${inter.className} flex items-center rounded-[20px] border border-blue-800 px-3 py-[2px] text-xs text-blue-800`}
-                >
-                  Фильтр 2
-                  <XMarkIcon width={24} height={24} className="ml-3" />
+              
+              </div>
+              <div className="flex justify-end">
+                <span className="mt-1 font-normal text-[14px] leading-5 text-[#6B7280]">125 оценок</span>
+              </div>
+             
+              
+              <div className="text-[#10B981] font-bold text-[30px] leading-9">10 000 ₽</div>
+
+
+              <div className="mt-4 flex justify-between gap-4">
+
+                <div className="flex gap-1">
+              
+                  <button className="bg-[#1E40AF] w-[180px] h-[40px] text-white rounded-[6px] flex items-center justify-center">
+                    <Image 
+                      src="/images/button_bag.svg" 
+                      alt="Корзина" 
+                      width={24} 
+                      height={24} 
+                      className="w-6 h-6"
+                    />
+                  </button>
+
+                  <button className="flex w-[40px] h-[40px] items-center justify-center">
+                    <Image 
+                      className="w-5 h-[18px] text-[#1E40AF] object-center object-contain" 
+                      src="/images/Heart.svg" 
+                      alt="Лайк" 
+                      width={20} 
+                      height={18} 
+                    />
+                  </button>
+                </div>
+
+                <p className="font-normal text-[16px] leading-6 text-[#6B7280] self-end">Есть в наличии</p>
+              </div>
+
+              <p className="mt-4">
+                Описание
+              </p>
+
+              <p className="mt-4 text-gray-600">
+                Описание Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Описание Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+
+              <h2 className="mt-6 text-lg font-bold">О товаре</h2>
+              <div className="mt-2 bg-gray-100 p-4 rounded-lg">
+                <div className="flex justify-between border-b pb-2">
+                  <span className="text-gray-600">Характеристика</span>
+                  <span className="font-bold">Значение</span>
+                </div>
+                <div className="flex justify-between border-b py-2">
+                  <span className="text-gray-600">Характеристика</span>
+                  <span className="font-bold">Значение</span>
+                </div>
+                <div className="flex justify-between border-b py-2">
+                  <span className="text-gray-600">Характеристика</span>
+                  <span className="font-bold">Значение</span>
+                </div>
+                <div className="flex justify-between pt-2">
+                  <span className="text-gray-600">Характеристика</span>
+                  <span className="font-bold">Значение</span>
                 </div>
               </div>
-  
-              <CatalogList variable="standart" items={items} photos={photos} />
-            </div>
-          </main>
+    
+            </main>
+          </div>
         </div>
       </>
     );
