610b69c5e3213356df013945836c71e13f3a2038
Bormxx <m2315226@edu.misis.ru>
2025-05-04 15:36:18 +0300

    Попытка всё сломать 9

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,7 @@ export default {
   out: "./migrations",
   dialect: "turso",
   dbCredentials: {
-    url: process.env.TURSO_DATABASE_URL!,
+    url: 'libsql://ecommerce-bormxx.aws-eu-west-1.turso.io',
     authToken: process.env.TURSO_AUTH_TOKEN!,
   },
 } satisfies Config;
