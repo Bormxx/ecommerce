feccb1032b5027950bb65a7317716ffb8910b687
Bormxx <m2315226@edu.misis.ru>
2025-05-04 15:51:33 +0300

    Попытка всё сломать 12

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,17 +1,15 @@
-import { defineConfig } from "drizzle-kit";
+import type { Config } from "drizzle-kit";
 import 'dotenv/config';
 
-export default defineConfig(
-  {
-    schema: "./src/api/models/**/*.ts",
-    out: "./migrations",
-    dialect: "turso",
-    dbCredentials: {
-      url: process.env.TURSO_DATABASE_URL!,
-      authToken: process.env.TURSO_AUTH_TOKEN!,
-    },
-  } 
-)
+export default {
+  schema: "./src/api/models/**/*.ts",
+  out: "./migrations",
+  dialect: "turso",
+  dbCredentials: {
+    url: process.env.TURSO_DATABASE_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
+  },
+} satisfies Config;
 
 
 // import type { Config } from "drizzle-kit";
