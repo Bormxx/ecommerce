e8ab1060698496f5d9d685b85ff995d1947e5852
Bormxx <m2315226@edu.misis.ru>
2025-05-04 16:34:17 +0300

    Попытка 17

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,17 @@
 import type { NextConfig } from "next";
 
-const cspHeader = ``
-
-    // default-src 'self';
-    // script-src 'self' 'unsafe-eval' 'unsafe-inline';
-    // style-src 'self' 'unsafe-inline';
-    // img-src *;
-    // font-src 'self';
-    // object-src 'none';
-    // base-uri 'self';
-    // form-action 'self';
-    // frame-ancestors 'none';
-    // upgrade-insecure-requests;
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src *;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -44,7 +44,8 @@ module.exports = {
           {
             key: 'Content-Security-Policy',
             value: cspHeader.replace(/\n/g, ''),
-          }
+          },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,PUT,DELETE' },
         ]
       }
     ]
