b66cb1158c121c3ad445db9de32094a27d5fa17c
Bormxx <m2315226@edu.misis.ru>
2025-05-06 18:00:13 +0300

    Check favorites

--- src/pages/api/products/favorites/index.ts
+++ src/pages/api/products/favorites/index.ts
@@ -19,7 +19,7 @@ export default async function Favorites(
           where: (item, { eq }) => eq(item.userId, user)
         })
         const likedItems = await Promise.all(favorites.map(item => getItemByIdHandler(item.itemId, res)))
-        return res.status(200).json({favorites, likedItems });
+        res.status(200).json({favorites, likedItems });
       } catch (error) {
         res.status(500).json({ access: "denied" });
       }
