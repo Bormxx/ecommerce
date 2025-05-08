e3bb007a8c99d4317bd3e97049fd077d8edbbf8b
Bormxx <m2315226@edu.misis.ru>
2025-05-08 11:48:56 +0300

    Добавил выбор базы данных

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,28 +1,39 @@
+// -----------------------------------------------------
+// Кроме этого файла редактируем ещё файл /src/api/db.ts
+// -----------------------------------------------------
+
 import type { Config } from "drizzle-kit";
 // import 'dotenv/config';
 require("dotenv").config();
 
-export default {
-  schema: "./src/api/models/**/*.ts",
-  out: "./migrations",
-  dialect: "turso",
-  dbCredentials: {
-    url: process.env.TURSO_DATABASE_URL!,
-    authToken: process.env.TURSO_AUTH_TOKEN!,
-  },
-} satisfies Config;
 
+// -------Выбираем тип базы данных-----------------------
 
-// import type { Config } from "drizzle-kit";
+// Этот блок для Turso, раскомментировать, если будете использовать Turso
 
 // export default {
 //   schema: "./src/api/models/**/*.ts",
-//   // dialect: 'turso',
-//   dialect: "sqlite",
+//   out: "./migrations",
+//   dialect: "turso",
 //   dbCredentials: {
-//     url: "dev.db",
-//     // url: `${process.env.TURSO_HOST}`,
-//     // authToken: `${process.env.TURSO_AUTH_TOKEN}`
+//     url: process.env.TURSO_DATABASE_URL!,
+//     authToken: process.env.TURSO_AUTH_TOKEN!,
 //   },
-//   out: "./drizzle",
 // } satisfies Config;
+
+// -----------------------------------------------------
+
+// Этот блок для Sqlite, раскомментировать, если будете использовать Sqlite
+
+export default {
+    schema: "./src/api/models/**/*.ts",
+    dialect: "sqlite",
+    dbCredentials: {
+        url: "dev.db",
+      },
+      out: "./drizzle",
+    } satisfies Config;
+
+// Если выбрана SQLite база данных, то сделать npm run push в консоли!!!
+// -----------------------------------------------------
+    
--- src/api/db.ts
+++ src/api/db.ts
@@ -1,7 +1,25 @@
-// import Database from "better-sqlite3";
-// import { drizzle } from "drizzle-orm/better-sqlite3";
-import { createClient } from "@libsql/client";
-import { drizzle } from "drizzle-orm/libsql";
+// -----------------------------------------------------
+// Кроме этого файла редактируем ещё drizzle.config.ts
+// -----------------------------------------------------
+
+// -------Выбираем тип базы данных----------------------
+
+// Для SQLite базы данных
+
+import Database from "better-sqlite3";
+import { drizzle } from "drizzle-orm/better-sqlite3";
+
+
+// Для Turso базы данных
+
+// import { createClient } from "@libsql/client";
+// import { drizzle } from "drizzle-orm/libsql";
+
+// В самом низу тоже нужно выбрать нужный тип базы данных
+// ------------------------------------------------------
+
+
+// Основные модели и схема базы данных
 import { basket } from "@/api/models/cart";
 import {
   lists,
@@ -43,11 +61,21 @@ const schema = {
   listsRelations,
 };
 
-const turso = createClient({
-  url: process.env.TURSO_DATABASE_URL!,
-  authToken: process.env.TURSO_AUTH_TOKEN!,
-});
-export const db = drizzle(turso, {schema});
+// -------Выбираем тип базы данных----------------------
 
-// const baza = new Database("dev.db");
-// export const db = drizzle(baza, { schema });
+// Для Turso базы данных
+
+// const turso = createClient({
+//   url: process.env.TURSO_DATABASE_URL!,
+//   authToken: process.env.TURSO_AUTH_TOKEN!,
+// });
+// export const db = drizzle(turso, {schema});
+
+
+// Для SQLite базы данных
+
+const baza = new Database("dev.db");
+export const db = drizzle(baza, { schema });
+
+// Если выбрана SQLite база данных, то сделать npm run push в консоли!!!
+// ------------------------------------------------------
