05508e4f2919aea39af24dbce23efe2097ec3eb9
MikleKuzichev <dev310105@gmail.com>
2025-05-22 18:04:03 +0300

    Fix: update to dev

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -9,28 +9,28 @@ import "dotenv/config";
 
 // Этот блок для Turso, раскомментировать, если будете использовать Turso
 
-export default {
-  schema: "./src/api/models/**/*.ts",
-  out: "./migrations",
-  dialect: "turso",
-  dbCredentials: {
-    url: process.env.TURSO_DATABASE_URL!,
-    authToken: process.env.TURSO_AUTH_TOKEN!,
-  },
-} satisfies Config;
+//export default {
+//  schema: "./src/api/models/**/*.ts",
+//  out: "./migrations",
+//  dialect: "turso",
+//  dbCredentials: {
+//    url: process.env.TURSO_DATABASE_URL!,
+//    authToken: process.env.TURSO_AUTH_TOKEN!,
+//  },
+//} satisfies Config;
 
 // -----------------------------------------------------
 
 // Этот блок для Sqlite, раскомментировать, если будете использовать Sqlite
 
-// export default {
-//   schema: "./src/api/models/**/*.ts",
-//   dialect: "sqlite",
-//   dbCredentials: {
-//     url: "dev.db",
-//   },
-//   out: "./drizzle",
-// } satisfies Config;
+ export default {
+   schema: "./src/api/models/**/*.ts",
+   dialect: "sqlite",
+   dbCredentials: {
+     url: "dev.db",
+   },
+   out: "./drizzle",
+ } satisfies Config;
 
 // Если выбрана SQLite база данных, то сделать npm run push в консоли!!!
 // -----------------------------------------------------
